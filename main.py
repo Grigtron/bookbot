9c98f70d70e00bce2d43efa97d65dd46a2d01797
Grigtron <jgregas@gmail.com>
2024-09-18 19:21:54 -0400

    Added word count

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+    words = text.split()
+    print("Word Count:", len(words))
+    
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()      
+
+
+    
+main()
