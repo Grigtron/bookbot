a4ce046ca2cd52798982bffca7080f7ff0d50ce0
Grigtron <jgregas@gmail.com>
2024-09-20 20:34:28 -0400

    Clarified character in variables instead of letter

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = word_count(text)
-    letter_string = letter_count(text)
+    char_string = char_count(text)
     print(text)
     print(f"Word Count: {num_words}")
-    print (letter_string)
+    print (char_string)
 
 ## Word Count of text
 def word_count(text):
@@ -23,9 +23,9 @@ def char_count(text):
     uniquechar_dict_lower = text.lower()
     for char in uniquechar_dict_lower:
         if char not in uniquechar_dict:
-            uniquechar_dict[letter] = 0
+            uniquechar_dict[char] = 0
 
-        uniquechar_dict[letter] += 1
+        uniquechar_dict[char] += 1
     return uniquechar_dict
     
 
