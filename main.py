63966f3a2a9f38d2e4df7910ed12a69f816dab4a
Grigtron <jgregas@gmail.com>
2024-09-21 17:00:51 -0400

    Added a better report output and sorted by most common occuring character

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ def main():
     text = get_book_text(book_path)
     num_words = word_count(text)
     char_string = char_count(text)
-    print(text)
+    sorted_dict = sort_char(char_string)
+    print(f"--- Begin report of {book_path} ---")
     print(f"Word Count: {num_words}")
-    print (char_string)
+    for char_dict in sorted_dict:
+        print(f"The '{char_dict['char']}' character was found {char_dict['count']} times")
 
 ## Word Count of text
 def word_count(text):
@@ -17,19 +19,26 @@ def get_book_text(path):
     with open(path) as f:
         return f.read() 
 
-## Unique letter count
+## Unique character count
 def char_count(text):
     uniquechar_dict = {}
     uniquechar_dict_lower = text.lower()
     for char in uniquechar_dict_lower:
-        if char not in uniquechar_dict:
-            uniquechar_dict[char] = 0
-
-        uniquechar_dict[char] += 1
+        if char.isalpha():
+            if char not in uniquechar_dict:
+                uniquechar_dict[char] = 1
+            else:
+                uniquechar_dict[char] += 1
     return uniquechar_dict
-    
 
-
+## Sort unique character count cleaner
+def sort_char(char_string):
+    sort_dic = []
+    for char, count in char_string.items():
+        char_dict = {"char": char, "count": count}
+        sort_dic.append(char_dict)
+    sort_dic.sort(key=lambda x: x ["count"], reverse=True)
+    return sort_dic
 
 
 
